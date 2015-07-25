f7173a10df83cf144ed684cebcfc32439910eb84
Byron Figueroa <brfigueroa@utpl.edu.ec>
2015-07-25 18:44:36 -0500

    Comentarios al codigo

--- twitterCliente/index.js
+++ twitterCliente/index.js
@@ -7,9 +7,10 @@ var topico1 = 'utpl/IntelGalileo/led';
 var topico2 = 'utpl/ArduinoMega/led';
 var mensaje1 = 'ON';
 var mensaje2 = 'OFF';
-
+//puerto en el cual esta el broker
 var puerto = 1883;
 
+//variables del apiKey de twitter
 var T = new Twit({
 	consumer_key : 'your consumer_key'
 	,consumer_secret: 'your consumer_secret'
@@ -25,27 +26,36 @@ console.log('|                                           |')
 console.log('|-------------------------------------------|')
 console.log('')
 
+//crea un nuevo cliente MQTT
 var client = mqtt.createClient(puerto, serverMqtt);
 client.options.reconnectPeriod = 0; 	
 
-
+//Busca al usuario con el ID 14585199 en twitter
 var stream = T.stream('statuses/filter', { follow: '14585199' });
 
+//cualquier mensaje que publique lo compara y si coincide publica el topico con el mensaje
 stream.on('tweet', function (tweet) {
   console.log(tweet.text + ' (' + tweet.user.screen_name  + ')');
+  //si el mensaje coincide con GalileoON publica al bkoker sobre el topico especificado.
   if (tweet.text.match('GalileoON')){
   	 console.log('mensaje coincide');
      client.publish(topico1, mensaje1);
      console.log('Publicando mensaje: utpl/IntelGalileo/led ON ')
-    }else if(tweet.text.match('GalileoOFF')){
+    }
+  //si el mensaje coincide con GalileoOFF publica al bkoker sobre el topico especificado.
+    else if(tweet.text.match('GalileoOFF')){
       console.log('mensaje coincide');
       client.publish(topico1, mensaje2);
       console.log('Publicando mensaje: utpl/IntelGalileo/led OFF ')
-    }else if (tweet.text.match('MegaON')){
+    }
+    //si el mensaje coincide con MegaON publica al bkoker sobre el topico especificado.
+    else if (tweet.text.match('MegaON')){
       console.log('mensaje coincide');
       client.publish(topico2, mensaje1);
       console.log('Publicando mensaje: utpl/ArduinoMega/led ON ')
-    }else if (tweet.text.match('MegaOFF')){
+    }
+    //si el mensaje coincide con MegaOFF publica al bkoker sobre el topico especificado.
+    else if (tweet.text.match('MegaOFF')){
       console.log('mensaje coincide');
       client.publish(topico2, mensaje2);
       console.log('Publicando mensaje: utpl/ArduinoMega/led OFF')
