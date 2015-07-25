d74027eaf8ed0889dff02c11a7869e7f03baf152
Byron Figueroa <brfigueroa@utpl.edu.ec>
2015-07-25 14:42:23 -0500

    Mejoras en el codigo

--- twitterCliente/index.js
+++ twitterCliente/index.js
@@ -1,16 +1,17 @@
 var Twit = require('twit')
 var mqtt = require('mqtt');
 // direccion en la cual esta el broker
-var serverMqtt = '192.168.0.12';
+var serverMqtt = '192.168.1.13';
+//Topicos a los cuales se van a publicar
 var topico1 = 'utpl/IntelGalileo/led';
-var topico1 = 'utpl/ArduinoMega/led';
+var topico2 = 'utpl/ArduinoMega/led';
 var puerto = 1883;
 
 var T = new Twit({
-	consumer_key : 'your consumer_key '
-	,consumer_secret: 'your consumer_secret '
+	consumer_key : 'your consumer_key'
+	,consumer_secret: 'your consumer_secret'
 	,access_token: 'your access_token'
-	,access_token_secret: 'your access_token_secret '
+	,access_token_secret: 'your access_token_secret'
 })
 
 
@@ -19,6 +20,7 @@ console.log('|                                           |')
 console.log('| MQTT CLIENTE conectado atraves de TWITTER |')
 console.log('|                                           |')
 console.log('|-------------------------------------------|')
+console.log('')
 
 var client = mqtt.createClient(puerto, serverMqtt);
 client.options.reconnectPeriod = 0; 	
@@ -30,19 +32,19 @@ stream.on('tweet', function (tweet) {
   console.log(tweet.text + ' (' + tweet.user.screen_name  + ')');
   if (tweet.text.match('GalileoON')){
   	 console.log('mensaje coincide');
-     client.publish('utpl/IntelGalileo/led', 'ON');
+     client.publish(topico1, 'ON');
      console.log('Publicando mensaje: utpl/IntelGalileo/led ON ')
     }else if(tweet.text.match('GalileoOFF')){
       console.log('mensaje coincide');
-      client.publish('utpl/IntelGalileo/led', 'OFF');
+      client.publish(topico1, 'OFF');
       console.log('Publicando mensaje: utpl/IntelGalileo/led OFF ')
     }else if (tweet.text.match('MegaON')){
       console.log('mensaje coincide');
-      client.publish('utpl/ArduinoMega/led', 'ON');
+      client.publish(topico2, 'ON');
       console.log('Publicando mensaje: utpl/ArduinoMega/led ON ')
     }else if (tweet.text.match('MegaOFF')){
       console.log('mensaje coincide');
-      client.publish('utpl/ArduinoMega/led', 'OFF');
+      client.publish(topico2, 'OFF');
       console.log('Publicando mensaje: utpl/ArduinoMega/led OFF')
     }
     else {
