f333b84907bf9c1d100ade9a0a46ff4f8329639f
Byron Figueroa <brfigueroa@utpl.edu.ec>
2015-07-25 18:35:40 -0500

    Nuevos cambios

--- twitterCliente/index.js
+++ twitterCliente/index.js
@@ -5,6 +5,9 @@ var serverMqtt = '192.168.1.13';
 //Topicos a los cuales se van a publicar
 var topico1 = 'utpl/IntelGalileo/led';
 var topico2 = 'utpl/ArduinoMega/led';
+var mensaje1 = 'ON';
+var mensaje2 = 'OFF';
+
 var puerto = 1883;
 
 var T = new Twit({
@@ -32,19 +35,19 @@ stream.on('tweet', function (tweet) {
   console.log(tweet.text + ' (' + tweet.user.screen_name  + ')');
   if (tweet.text.match('GalileoON')){
   	 console.log('mensaje coincide');
-     client.publish(topico1, 'ON');
+     client.publish(topico1, mensaje1);
      console.log('Publicando mensaje: utpl/IntelGalileo/led ON ')
     }else if(tweet.text.match('GalileoOFF')){
       console.log('mensaje coincide');
-      client.publish(topico1, 'OFF');
+      client.publish(topico1, mensaje2);
       console.log('Publicando mensaje: utpl/IntelGalileo/led OFF ')
     }else if (tweet.text.match('MegaON')){
       console.log('mensaje coincide');
-      client.publish(topico2, 'ON');
+      client.publish(topico2, mensaje1);
       console.log('Publicando mensaje: utpl/ArduinoMega/led ON ')
     }else if (tweet.text.match('MegaOFF')){
       console.log('mensaje coincide');
-      client.publish(topico2, 'OFF');
+      client.publish(topico2, mensaje2);
       console.log('Publicando mensaje: utpl/ArduinoMega/led OFF')
     }
     else {
